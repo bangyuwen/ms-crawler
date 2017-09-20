ebf068b2cea213386a0d0821896dca284dd9b99d
BangYuWen <bangyuwen@gmail.com>
2017-09-20 17:44:29 +0800

    get data v0.0.1

---
+++ unit-test.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+let store1m = []
+let store3m = []
+let store = {}
+
+let fetch = () => {
+  request('http://monnsutogatya.com/', (error, response, body) => {
+    let $ = cheerio.load(body)
+    $('div#tab_1m tr td span').each(function(i, elem) {
+      if (i % 2 === 0) store1m[i / 2] = $(this).text().replace(/\s/g, '')
+    })
+    $('div#tab_3m tr td span').each(function(i, elem) {
+      if (i % 2 === 0) store3m[i / 2] = $(this).text().replace(/\s/g, '')
+    })
+    store = {
+      date: new Date(),
+      oneMin: {
+        times: [parseInt(store1m[0]), parseInt(store1m[1])],
+        probability: parseFloat(store1m[2])/100 || 0
+      },
+      threeMin: {
+        times: [parseInt(store3m[0]), parseInt(store3m[1])],
+        probability: parseFloat(store3m[2])/100 || 0
+      }
+    }
+    console.log(store)
+  })
+}
+
+fetch()
+setInterval(fetch, 30000)
